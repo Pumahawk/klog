18d1b8767c90017a96d69368b6647e381b6f5313
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-03-19 07:42:23 +0100

    Add yaml loading configuration

--- config.go
+++ config.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	yaml "gopkg.in/yaml.v3"
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 type TemplateVars = map[string]any
@@ -25,15 +27,24 @@ type LogConfig struct {
 
 func LoadConfig() (*Config, error) {
 	var filename = GlobalFlags.ConfigPath
+	isYaml, _ := regexp.MatchString("\\.yaml$", filename)
+	var confLoader ConfLoader
+	if isYaml {
+		confLoader = yaml.Unmarshal
+	} else {
+		confLoader = json.Unmarshal
+	}
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("errore nella lettura del file: %v", err)
 	}
 
 	var config Config
-	if err := json.Unmarshal(data, &config); err != nil {
+	if err := confLoader(data, &config); err != nil {
 		return nil, fmt.Errorf("errore nel parsing del JSON: %v", err)
 	}
 
 	return &config, nil
 }
+
+type ConfLoader = func(in []byte, out interface{}) (err error);
